f54cf7475fcfa997f9d704f998a881965b7475ad
nguyendoanphu2310@gmail.com <nguyendoanphu2310@gmail.com@890512f3-1735-9ab7-8796-2807aab7b740>
2012-10-22 15:57:22 +0000

    Thêm Class Object.cs Chuẩn bị tạo DxScreen và các Screens cần thiết cho game

---
+++ trunk/GameDirectXDemo/GameDirectXDemo/GameDirectXDemo/Object.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameDirectXDemo
+{
+    public class Object
+    {
+        public enum Type
+        {
+            Tanker,
+            Ranger,
+            Defender
+        }
+
+        public enum Side
+        {
+            Enemy,
+            Player
+        }
+        protected int _hp = 0;
+        protected int _stamina = 0;
+        protected int _damage = 0;
+        protected int _shield = 0;
+        protected int _shieldRestorePoint = 0;
+        protected Type _type;
+
+        public Object()
+        {
+        }
+
+        public virtual void Move() { }
+
+        public virtual void Attack() { }
+
+        public virtual void Die() { }
+
+        protected  virtual void Update()
+        {
+        
+        }
+
+        public virtual void Draw()
+        { }
+
+
+
+    }
+}
