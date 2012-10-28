9fdb07859d149fb83d39d0af7afd7cc3013277af
nguyendoanphu2310@gmail.com <nguyendoanphu2310@gmail.com@890512f3-1735-9ab7-8796-2807aab7b740>
2012-10-28 03:10:45 +0000

    Add Global

---
+++ trunk/GameDirectXDemo/GameDirectXDemo/GameDirectXDemo/Global.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameDirectXDemo
+{
+    public class Global
+    {
+        public enum BitmapType{ SOLID = 0,TRANSPARENT = 1}
+        
+       
+        public enum CursorState{Normal,Highlight}
+
+        public enum ObjectType{Tanker,Ranger,Defender}
+
+        public enum Side{Enemy,Player}
+
+        //public static void SetEnum(Enum e,ref Type t) 
+        //{
+            
+        //}
+    }
+}
