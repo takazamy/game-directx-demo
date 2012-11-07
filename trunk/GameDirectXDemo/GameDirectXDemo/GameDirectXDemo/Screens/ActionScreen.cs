1511d51f3c48b7af59575d30b5daacaaebc39d6b
nguyendoanphu2310@gmail.com <nguyendoanphu2310@gmail.com@890512f3-1735-9ab7-8796-2807aab7b740>
2012-11-07 17:41:03 +0000

    Add and Update Screens

---
+++ trunk/GameDirectXDemo/GameDirectXDemo/GameDirectXDemo/Screens/ActionScreen.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GameDirectXDemo.Core;
+using GameDirectXDemo.Manager;
+using System.Drawing;
+using Microsoft.DirectX.DirectInput;
+
+namespace GameDirectXDemo.Screens
+{
+    public class ActionScreen:DxScreen
+    {
+        public ActionScreen(ScreenManager scrManager, DxInitGraphics graphics, Point location, Size size) :
+            base(scrManager, graphics, location, size)
+        {
+
+        }
+
+        public override void  Initialize()
+        {
+ 	         base.Initialize();
+        }
+
+        public override void Update(double deltaTime,KeyboardState keyState, MouseState mouseState)
+        {
+            base.Update(deltaTime, keyState, mouseState);
+        }
+
+        public override void Draw()
+        {
+            base.Draw();
+        }
+    }
+}
