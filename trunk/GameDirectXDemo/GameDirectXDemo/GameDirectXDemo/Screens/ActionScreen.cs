76fc1a33b31800e9841ed8fc51489b2d4a0a88ea
nguyendoanphu2310@gmail.com <nguyendoanphu2310@gmail.com@890512f3-1735-9ab7-8796-2807aab7b740>
2012-11-08 16:18:46 +0000

    Update Screens

--- trunk/GameDirectXDemo/GameDirectXDemo/GameDirectXDemo/Screens/ActionScreen.cs
+++ trunk/GameDirectXDemo/GameDirectXDemo/GameDirectXDemo/Screens/ActionScreen.cs
@@ -6,30 +6,46 @@ using GameDirectXDemo.Core;
 using GameDirectXDemo.Manager;
 using System.Drawing;
 using Microsoft.DirectX.DirectInput;
+using Microsoft.DirectX.DirectDraw;
 
 namespace GameDirectXDemo.Screens
 {
     public class ActionScreen:DxScreen
     {
-        public ActionScreen(ScreenManager scrManager, DxInitGraphics graphics, Point location, Size size) :
+        DxButton move;
+        DxButton attack;
+        DxButton endTurn;
+        Surface parent;
+        DxImage bg;
+        public Boolean isShow = false;
+        public ActionScreen(ScreenManager scrManager, DxInitGraphics graphics, Point location, Size size,Surface gameScreenSurf) :
             base(scrManager, graphics, location, size)
         {
-
+            parent = gameScreenSurf;
+            Initialize();
         }
 
         public override void  Initialize()
         {
  	         base.Initialize();
+             bg = new DxImage(GameResource.ActionScreen, Global.BitmapType.SOLID, 0, _graphics.DDDevice);
+             move = new DxButton(5, 5, GameResource.move, _graphics.DDDevice, GameResource.move.Width, GameResource.move.Height/3);
         }
 
         public override void Update(double deltaTime,KeyboardState keyState, MouseState mouseState)
         {
             base.Update(deltaTime, keyState, mouseState);
         }
-
         public override void Draw()
         {
-            base.Draw();
+            if (isShow)
+            {
+                bg.DrawImage(this.Surface);
+                move.DrawFast(this.Surface);
+                parent.Draw(new Rectangle(this.Location, this.Size), this.Surface, DrawFlags.Wait);
+            }           
+            //base.Draw();
         }
+        
     }
 }
