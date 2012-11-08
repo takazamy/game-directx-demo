9ddc1f1b4f68387e8c049e0f3d72ea3f413ef039
nguyendoanphu2310@gmail.com <nguyendoanphu2310@gmail.com@890512f3-1735-9ab7-8796-2807aab7b740>
2012-11-08 16:19:42 +0000

    Update GameManager

--- GameDirectXDemo/GameDirectXDemo/GameDirectXDemo/Manager/GameManager.cs
+++ GameDirectXDemo/GameDirectXDemo/GameDirectXDemo/Manager/GameManager.cs
@@ -47,15 +47,8 @@ namespace GameDirectXDemo.Manager
             _scrManager.Append(new SplashScreen(_scrManager, _graphics, Point.Empty,
               new Size(800, 600),
               5000));
-            //_scrManager.UpdateIndex();
+            
             _scrManager.CurrentScreen = _scrManager.Children[0];
-            //
-            //_scrManager.Append(new InstructionScreen(_scrManager, _graphics, Point.Empty, new Size(800, 600)));
-            //_scrManager.Append(new CreditScreen(_scrManager, _graphics, new Point(200, 300), new Size(400, 300)));
-            //string s = "Assets/level01.xml";
-            //_scrManager.Append(new GameScreen(_scrManager, _graphics, Point.Empty, new Size(800, 600), new Core.DxImage("Assets/map1.jpg", _graphics.DDDevice), s));
-            //_scrManager.Append(new EndGameScreen(_scrManager, _graphics, Point.Empty, new Size(800, 600)));
-
         }
 
         public void Update(double deltaTime, KeyboardState keyState, MouseState mouseState)
