2e0a8830cae9f002931b517592a7431b4c86826a
nguyendoanphu2310@gmail.com <nguyendoanphu2310@gmail.com@890512f3-1735-9ab7-8796-2807aab7b740>
2012-11-09 18:11:01 +0000

    Update RandomPostion

--- GameDirectXDemo/GameDirectXDemo/GameDirectXDemo/Screens/GameScreen.cs
+++ GameDirectXDemo/GameDirectXDemo/GameDirectXDemo/Screens/GameScreen.cs
@@ -14,12 +14,16 @@ namespace GameDirectXDemo.Screens
         DxTileMap tileMap;
         DxCamera camera;
         List<Object> objects;
+        List<Object> PlayerList;
+        List<Object> EnemyList;
         ActionScreen actionScreen;
         Global.Turn gameTurn = Global.Turn.PlayerTurn;
+        int[,] colisionMap;
         public GameScreen(ScreenManager scrManager, DxInitGraphics graphics, Point location, Size size,List<Object> objects,DxTileMap tileMap) :
             base(scrManager, graphics, location, size)
         {
             this.tileMap = tileMap;
+            colisionMap = tileMap.CollisionMap;
             this.objects = objects;
 
             Initialize();
@@ -31,17 +35,77 @@ namespace GameDirectXDemo.Screens
             this._state = Global.ScreenState.GS_MAIN_GAME;
             camera = new DxCamera(Point.Empty, this.Size, this.tileMap.TileMapSurface, _graphics.DDDevice);
             actionScreen = new ActionScreen(_scrManager, _graphics, Point.Empty, new Size(60, 95), this.Surface);
+            EnemyList = new List<Object>();
+            PlayerList = new List<Object>();
+            foreach (Object obj in objects)
+            {
+                if (obj.Side == Global.Side.Enemy)
+                {
+                    EnemyList.Add(obj);
+                }
+                else
+                {
+                    PlayerList.Add(obj);
+                }
+            }
             CreateGame();
         }
 
         private void CreateGame()
         {
-           
+            RandomPostion();
         }
-
+        
         private void RandomPostion()
         {
             // chọn khu vực để tạo vị trí random cho quân
+            try
+            {
+                int row, col;
+                Random rand = new Random();
+                int temp = 0;
+                for (int i = 0; i < PlayerList.Count; i++)
+                {
+                    while (true)
+                    {
+                        temp = rand.Next(0, 4);
+                        row = temp;
+                        temp = rand.Next(0, 10);
+                        col = temp;
+
+                        if (colisionMap[row, col] == 0)
+                        {
+                            PlayerList[i].Position = new PointF((float)col * 32, (float)row * 32);//32 = frameWidth 
+                            colisionMap[row, col] = -1;
+                            break;
+                        }
+                    }
+
+                }
+                temp = 0;
+                for (int i = 0; i < EnemyList.Count; i++)
+                {
+                    while (true)
+                    {
+                        temp = rand.Next(18, 22);
+                        row = temp;
+                        temp = rand.Next(25, 34);
+                        col = temp;
+
+                        if (colisionMap[row, col] == 0)
+                        {
+                            EnemyList[i].Position = new PointF((float)col * 32, (float)row * 32);//32 = frameWidth 
+                            colisionMap[row, col] = -2;
+                            break;
+                        }
+                    }
+
+                }
+            }
+            catch(Exception ex)
+            {
+                
+            }
            
         }
 
@@ -51,10 +115,10 @@ namespace GameDirectXDemo.Screens
             {
                 foreach (Object obj in this.objects)
                 {
-                    if (obj.Side == Global.Side.Player)
-                    {
+                    //if (obj.Side == Global.Side.Player)
+                    //{
                         obj.Update(deltaTime, keyState, mouseState);
-                    }
+                    //}
                 }
             }
             camera.Update(keyState);
