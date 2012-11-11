0010ea36968e755a50e1db91e2f814d832abea2a
nguyenthelan89@gmail.com <nguyenthelan89@gmail.com@890512f3-1735-9ab7-8796-2807aab7b740>
2012-11-11 07:21:02 +0000

    Add CreditScreen, HowToPlayScreen. Update MenuScreen

---
+++ trunk/GameDirectXDemo/GameDirectXDemo/GameDirectXDemo/Screens/CreditScreen.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using Microsoft.DirectX.DirectDraw;
+using GameDirectXDemo.Core;
+using GameDirectXDemo.Manager;
+
+namespace GameDirectXDemo.Screens
+{
+    class CreditScreen:DxScreen
+    {
+        //DxButton _exitBtn = null;
+        Rectangle _destRect;
+        public CreditScreen(ScreenManager scrManager, DxInitGraphics graphics, Point location, Size size) :
+            base(scrManager, graphics, location, size)
+        {
+            Initialize();
+            
+        }
+        public override void Initialize()
+        {
+            base.Initialize(); 
+           
+            this._state = Global.ScreenState.GS_CREDIT;
+            _destRect = new Rectangle(_location, _size);
+
+        }
+        public void Draw(Surface destSurface)
+        {
+            this._surface.ColorFill(Color.White);
+            this._surface.DrawText(0, 0, "Desinger", false);
+            destSurface.Draw(_destRect,_surface, DrawFlags.Wait);
+        }
+    }
+}
---
+++ trunk/GameDirectXDemo/GameDirectXDemo/GameDirectXDemo/Screens/HowToPlayScreen.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using Microsoft.DirectX.DirectDraw;
+using GameDirectXDemo.Core;
+using GameDirectXDemo.Manager;
+
+namespace GameDirectXDemo.Screens
+{
+    class HowToPlayScreen:DxScreen
+    {
+        Rectangle _destRect;
+        public HowToPlayScreen(ScreenManager scrManager, DxInitGraphics graphics, Point location, Size size)
+            : base(scrManager, graphics, location, size)
+        {
+            Initialize();
+            
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            this._state = Global.ScreenState.GS_HELP;
+            _destRect = new Rectangle(_location, _size);
+        }
+        public void Draw(Surface destSurface)
+        {
+            this._surface.ColorFill(Color.Yellow);
+            this._surface.DrawText(0, 0, "How to play", false);
+            destSurface.Draw(_destRect, _surface, DrawFlags.Wait);
+        }
+    }
+}
