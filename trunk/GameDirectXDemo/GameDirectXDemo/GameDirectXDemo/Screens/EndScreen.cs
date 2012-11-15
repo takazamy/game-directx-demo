929ef516db4ad096c548c12f4d9ce493d908ee88
nguyendoanphu2310@gmail.com <nguyendoanphu2310@gmail.com@890512f3-1735-9ab7-8796-2807aab7b740>
2012-11-15 08:08:17 +0000

    Update xử lý cho turn Add EndScreen

---
+++ trunk/GameDirectXDemo/GameDirectXDemo/GameDirectXDemo/Screens/EndScreen.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GameDirectXDemo.Core;
+using GameDirectXDemo.Manager;
+using System.Drawing;
+
+namespace GameDirectXDemo.Screens
+{
+    class EndScreen:DxScreen
+    {
+        public EndScreen(ScreenManager scrManager, DxInitGraphics graphics, Point location, Size size, GameScreen gameScreen) :
+            base(scrManager, graphics, location, size)
+        { 
+
+        }
+    }
+}
